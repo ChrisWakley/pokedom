21e881032eb84c053c9d686df11d17f34f28f497
Chris Wakley <chris.wakley666@gmail.com>
2022-10-04 18:49:14 +0100

    testing card layout

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 import pokemonArray from "./data/pokemon.js";
+//console.log(pokemonArray);
 
-console.log(pokemonArray);
+const cardContainer = document.querySelector(".card-container");
+
+const showPokemon = () => {
+    pokemonArray.forEach((pokemon) => {
+    const name = pokemon.name;
+    const types = pokemon.types.join(" & ");
+    cardContainer.innerHTML +=
+    `<div class="card">
+            <img src="${pokemon.sprite}" class="card__image">
+            <div class="card__content">
+                <div class="card__heading">${name}</div>
+                <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
+            </div>
+    </div>`;
+});
+};
+
+showPokemon(pokemonArray);
